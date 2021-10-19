af2fe18a3346e5d55525f66ba53c97ba29dcfb6c
klburks <90107031+klburks@users.noreply.github.com>
2021-10-18 20:28:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+//Question 1
+const webDev = {
+    frame: "HTML",
+    style: "CSS",
+    logic: "JavaScript",
+}
+
+//Question 2
+const pizza = {
+    crust: "thin",
+    setCrust: function (crustType) {
+        
+    }
+
+}
+
+
+
+//Question 3
+const car = {
+    color: white,
+    make: Buick,
+}
+
+function doesItContain(key, obj) {
+    let hasKey = (obj[key] !==undefined);
+    console.log(hasKey);
+     
+}
+
+
+
+
+//Question 4
+function introduceTheActors(arr){
+
+
+}
+
+
+
+//Question 5
+let techCompanies = {
+    Microsoft: "Bill Gates",
+    Amazon: "Jeff Bezos",
+    Tesla: "Elong Musk",
+    Facebook: "Mark Zuckerberg",
+    Apple: "Steve Jobs",
+    founderLookup: function (companyName) {
+        Object.values(techCompanies).forEach(item => {
+            console.log(item);
+        });
+
+    }
+
+
+
+}
+
+
+
+//Question 6
+
+
+//Question 7
+
+//Question 8a
+
+
+//Question 8b
